95b2971c06dc8453b83e14582fc898cd53e15627
Daniel Ochoa <daniel.romero48@uabc.edu.mx>
2022-06-16 12:24:21 -0700

    Proptypes y demás

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({ title, subTitle }) => {
+
+  return (
+    <>
+    <h1>{ title }</h1>
+    <h4>{subTitle}</h4>
+    </>
+  )
+}
+
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.number.isRequired,
+}
