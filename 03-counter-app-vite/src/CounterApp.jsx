fb67e782a175ed0b18bc8c4554cd60ccaf446fe6
danielr8ochoa <daniel.romero48@uabc.edu.mx>
2022-06-17 23:17:21 -0700

    Primer componente: CounterApp

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,17 +1,32 @@
+import { useState  } from 'react';
 import PropTypes from 'prop-types';
 
-export const CounterApp = ({
-  value
-}) => {
+
+export const CounterApp = ({ value }) => {
+
+  const [ counter, setCounter ]  = useState( value );
+
+  
+  const handleAdd = () => { 
+    setCounter( counter + 1 );
+  }
+
+  const handleSubstract = () => { 
+    setCounter( counter - 1 );
+  }
+
+  const handleReset = () => { 
+    setCounter( value );
+  }
 
   return (
     <>
     <h1>CounterApp</h1>
-    <h2>{ value }</h2>
+    <h2> { counter } </h2>
 
-    <button onClick={ function(){ console.log('+1')} }>
-      +1
-    </button>
+    <button onClick={ handleAdd }> +1 </button>
+    <button onClick={ handleSubstract }> -1 </button>
+    <button onClick={ handleReset }> Reset  </button>
     </>
   )
 }
